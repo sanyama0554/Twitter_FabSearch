cb5ab3980a0b35052b001830a9cb01ce5b74e7d2
sanyama0554 <sanyama554@gmail.com>
2023-01-02 12:34:28 +0900

    Mainコンポーネント追加

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { css } from "@emotion/react";
+
+const Main: React.FC = () => {
+  return(
+    <main>
+      <p>ユーザー絞り込み部分</p>
+      <p>メイン部分</p>
+    </main>
+  )
+}
+
+export default Main
